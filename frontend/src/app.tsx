bdf290ecd01320fa4987f0e19e7a264d6c36abff
oevadee <a.szczechura.98@gmail.com>
2021-05-11 18:25:33 +0200

    added ui for login Route

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,12 +1,24 @@
+import { Box, makeStyles } from "@material-ui/core";
 import { Router, Route } from "preact-router";
-import {LoginRoute, HomeRoute} from "./routes";
+import { LoginRoute, HomeRoute } from "./routes";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    border: "solid 1px blue",
+    height: "100vh",
+    width: "100vw",
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <Router>
-      <Route path="/" component={HomeRoute} />
-      <Route path="/login" component={LoginRoute} />
-    </Router>
+    <Box className={classes.root}>
+      <Router>
+        <Route path="/" component={HomeRoute} />
+        <Route path="/login" component={LoginRoute} />
+      </Router>
+    </Box>
   );
 };
 
--- frontend/src/routes/LoginRoute/LoginRoute.tsx
+++ frontend/src/routes/LoginRoute/LoginRoute.tsx
@@ -1,9 +1,62 @@
-const LoginRoute = () => {
-  return (
-    <div>
-      LoginRoute
-    </div>
-  )
-}
+import {
+  Box,
+  Button,
+  makeStyles,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 
-export default LoginRoute
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  formInput: {
+    width: "100%",
+    marginTop: theme.spacing(4),
+  },
+  formWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end",
+  },
+  passwordInput: {
+    width: "100%",
+    margin: theme.spacing(2, 0),
+  },
+}));
+
+const LoginRoute = () => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      <Typography variant="h2">Stream-hat</Typography>
+      <Typography variant="h5">
+        Livestream your screen and share it with friends.
+      </Typography>
+      <Box width={500} className={classes.formWrapper}>
+        <TextField
+          placeholder="email"
+          className={classes.formInput}
+          variant="outlined"
+          color="secondary"
+        />
+        <TextField
+          placeholder="password"
+          className={classes.passwordInput}
+          variant="outlined"
+          color="secondary"
+        />
+        <Button variant="contained" color="primary">
+          SignIn
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginRoute;
